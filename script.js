67debc935074c7cfc47af1e9c02aae4d7e55a2ad
WillBeFront-Enduser <pugbmobile40@gmail.com>
2023-01-16 22:02:46 +0300

    afsaf

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// Get the input and messages elements
 const input = document.getElementById('chat-input');
 const messages = document.getElementById('chat-messages');
 
-// Add the send message event listener
 document.getElementById('chat-send').addEventListener('click', () => {
-  // Create a new message element
   const message = document.createElement('div');
   message.innerText = input.value;
   message.classList.add("message");
 
-  // Add the message to the messages container
   messages.appendChild(message);
 
-  // Clear the input field
   input.value = '';
 
-  // Scroll to the bottom of the messages container
   messages.scrollTop = messages.scrollHeight;
 });
