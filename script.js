bcff94250dd2e2ab4405773a653a096a2d334015
WillBeFront-Enduser <pugbmobile40@gmail.com>
2023-01-16 22:29:41 +0300

    andg

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const input = document.getElementById('chat-input');
-const messages = document.getElementById('chat-messages');
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("chat-messages");
+const sendButton = document.getElementById("chat-send");
 
-document.getElementById('chat-send').addEventListener('click', () => {
-  const message = document.createElement('div');
-  message.innerText = input.value;
+sendButton.addEventListener("click", () => {
+  if (!input.value.trim()) {
+    return;
+  }
+  const message = document.createElement("div");
+  message.innerHTML = input.value;
   message.classList.add("message");
-
   messages.appendChild(message);
-
-  input.value = '';
-
+  input.value = "";
   messages.scrollTop = messages.scrollHeight;
 });
